3f0425db11474f84d096763c84e64fac6a4472dd
samuel <samuel@samuel.baker.ca>
2023-05-10 23:25:08 -0700

    - added tailwind - added navbar component

---
+++ my-app/src/components/navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import 
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ol className='navbar'>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/about">Contact</Link>
+        </li>
+        <li>
+          <Link to="/about">Projects</Link>
+        </li>
+      </ol>
+    </nav>
+  );
+};
+
+
+export default Navbar;
