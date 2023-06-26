3d6f46e9eb9b085e6bc60900e9998fa8e96d11c1
samuel <samuel.baker@rack-a-tiers.com>
2023-06-26 13:15:33 -0700

    - resized project images

--- my-app/src/components/portfolio.js
+++ my-app/src/components/portfolio.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import project1 from "../images/React.jpg";
+import Image1 from '../images/React.jpg';
+import Image2 from '../images/dawdadw.png';
+import Image3 from '../images/delldock.jpg';
+import Image4 from '../images/inventorscorner.png';
+import Image5 from '../images/phone.png';
+import Image6 from '../images/step1.png';
 
 const Portfolio: React.FC = () => {
   const [activeView, setActiveView] = useState("all");
@@ -11,12 +16,14 @@ const Portfolio: React.FC = () => {
 
   // project data
   const projects = [
-    { id: 1, title: "Project 1", category: "all" },
-    { id: 2, title: "Project 2", category: "all" },
-    { id: 3, title: "Project 3", category: "python" },
-    { id: 4, title: "Project 4", category: "react" },
-    { id: 5, title: "Project 5", category: "react" },
-    { id: 6, title: "Project 6", category: "react" },
+    { id: 1, title: "Project 1", category: "all", image: Image1 },
+    { id: 2, title: "Project 2", category: "all", image: Image2 },
+    { id: 3, title: "Project 3", category: "react", image: Image3 },
+    { id: 4, title: "Project 4", category: "python", image: Image4 },
+    { id: 5, title: "Project 5", category: "python", image: Image5},
+    { id: 6, title: "Project 6", category: "react", image: Image6 },
+
+    // Add more projects and their respective images here
   ];
 
   const filteredProjects = projects.filter((project) => {
@@ -29,11 +36,7 @@ const Portfolio: React.FC = () => {
 
   return (
     <div className="mx-auto p-6 container" id="portfolio">
-      
-      {/* title for div */}
       <h1 className="text-3xl text-center font-bold mb-4">Portfolio</h1>
-      
-      {/* filter buttons for projects */}
       <div className="flex justify-center mb-4">
         <button
           className={`btn btn-primary mr-2 ${
@@ -60,30 +63,32 @@ const Portfolio: React.FC = () => {
           React
         </button>
       </div>
-
-      {/* project template*/}
-      <motion.div
-        className="grid grid-cols-1 md:grid-cols-3 gap-8"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.2, duration: 0.8 }}
-      >
-        {filteredProjects.map((project) => (
-          <div key={project.id}>
-            <motion.div
-              className="shadow rounded-lg"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-            >
-              <img src={project1} alt={project.title} />
-            </motion.div>
-          </div>
-        ))}
-      </motion.div>
-
+      <div>
+        <motion.div
+          className="grid grid-cols-1 md:grid-cols-3 gap-8"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2, duration: 0.8 }}
+        >
+          {filteredProjects.map((project) => (
+            <div key={project.id} className="flex items-center justify-center">
+              <motion.div
+                className="shadow-2xl rounded-lg w-64 h-48 md:w-96 md:h-64"
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="object-cover w-full h-full"
+                />
+              </motion.div>
+            </div>
+          ))}
+        </motion.div>
+      </div>
     </div>
   );
 };
 
 export default Portfolio;
-
