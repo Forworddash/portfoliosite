8019a342d6285e144c3708696ca8a8ae1a53a039
samuel <samuel.baker@rack-a-tiers.com>
2023-06-27 16:03:36 -0700

    - about div orders

--- my-app/src/components/about.js
+++ my-app/src/components/about.js
@@ -7,12 +7,12 @@ const About: React.FC = () => {
       <div>
         <h1 className="text-3xl text-center font-bold mb-4">About</h1>
       </div>
-      <div className='grid grid-cols-2'>
-        <div className='m-auto'>
+      <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2'>
+        <div className='m-auto order-2 xl:order-1 lg:order-1 md:order-2 sm:order-2'>
           <img src='https://cdn.rack-a-tiers.com/wp-content/uploads/2022/10/03100805/Samuel-B-Second-Version.jpg' alt='profile picture' className='w-64 h-auto rounded-full'></img>
         </div>
 
-        <div className=''>
+        <div className='order-1 xl:order-2 lg:order-2 md:order-1 sm:order-1'>
           <p className="text-lg leading-7">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id magna
             consequat, cursus nulla sit amet, accumsan velit. Quisque interdum
