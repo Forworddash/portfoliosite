857a7b378c71ba760f4155d7a816dd53958d7a78
samuel <samuel.baker@rack-a-tiers.com>
2023-06-29 15:28:57 -0700

    - hide picture on about for mobile

--- my-app/src/components/about.js
+++ my-app/src/components/about.js
@@ -8,7 +8,7 @@ const About: React.FC = () => {
         <h1 className="text-3xl text-center font-bold mb-4">About</h1>
       </div>
       <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2'>
-        <div className='m-auto order-2 xl:order-1 lg:order-1 md:order-2 sm:order-2'>
+        <div className='m-auto order-2 xl:order-1 lg:order-1 md:order-2 sm:order-2 hidden xl:block lg:block'>
           <img src='https://cdn.rack-a-tiers.com/wp-content/uploads/2022/10/03100805/Samuel-B-Second-Version.jpg' alt='profile picture' className='w-64 h-auto rounded-full shadow-2xl'></img>
         </div>
 
