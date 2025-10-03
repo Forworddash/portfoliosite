bc3aeef0750350af56e461341c3595e74b402ba1
forworddash <forworddash@users.noreply.github.com>
2025-10-02 21:28:04 -0700

    [UPD]

--- src/components/about.js
+++ src/components/about.js
@@ -7,10 +7,10 @@ const About = () => {
       <div>
         <h1 className="text-3xl text-center font-bold mb-4">About</h1>
       </div>
-      <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2'>
-        <div className='m-auto order-2 xl:order-1 lg:order-1 md:order-2 sm:order-2 hidden xl:block lg:block'>
+      <div className='grid grid-cols-1'>
+        {/* <div className='m-auto order-2 xl:order-1 lg:order-1 md:order-2 sm:order-2 hidden xl:block lg:block'>
           <img src='https://cdn.rack-a-tiers.com/wp-content/uploads/2022/10/03100805/Samuel-B-Second-Version.jpg' alt='profile picture' className='w-64 h-auto rounded-full shadow-2xl'></img>
-        </div>
+        </div> */}
 
         <div className='order-1 xl:order-2 lg:order-2 md:order-1 sm:order-1 rounded-2xl m-auto shadow-2xl p-8'>
           <p className="text-lg leading-7">
